51a999c656e0e5cbf56427022248c07ceebf83ae
Jeremy Meyer <jjman505@gmail.com>
2016-04-14 12:42:39 -0400

    add new service ideas

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,13 @@
 'use strict';
+const idea = require('./idea');
 const authentication = require('./authentication');
 const user = require('./user');
 
 module.exports = function() {
   const app = this;
-  
-  
+
+
   app.configure(authentication);
   app.configure(user);
+  app.configure(idea);
 };
