5393e9ec9398d7f5131802a48c52f646292d757e
Jeremy Meyer <jeremythedev@gmail.com>
2016-12-16 01:04:12 -0800

    feat: update Home

--- src/client/components/Home/Home.js
+++ src/client/components/Home/Home.js
@@ -1,52 +1,19 @@
 import React, { Component } from 'react';
-import { Lists } from '../lists';
+import { connect } from 'react-redux'
+import _ from 'lodash';
+import { Topics } from '../topics';
 import { app } from '../../app';
 
-// export class Home extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       lists: []
-//     }
-//     this.listInput = null;
-//     this.createList = this.createList.bind(this);
-//   }
-//   componentDidMount() {
-//     const listService = app.service('lists');
-//     const query = { query: { $sort: {createdAt: -1 } } };
-//     listService.find(query)
-//     .then(list => this.setState({ lists: list.data }));
-//     listService.on('created', list => this.setState({ lists: this.state.lists.concat(list) }));
-//   }
-//   createList(event) {
-//     event.preventDefault();
-//     const listService = app.service('lists');
-//     listService.create({
-//       name: this.listInput.value
-//     }).then(() => this.listInput.value = '');
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <form onSubmit={this.createList}>
-//           <input type="text" placeholder="Enter new list" ref={(list) => this.listInput = list} />
-//           <button type="submit">Create</button>
-//         </form>
-//         <Lists lists={this.state.lists}/>
-//       </div>
-//     )
-//   }
-// };
+const topicUrl = (topic) => `/topics/${topic._id}`;
 
-const Home = (props) => (
-  <div class="main-container">
+const mapStateToProps = (state, ownProps) => _.pick(state, 'topics');
+const mapDispatchToProps = null;
+
+const HomePresenter = (props) => (
+  <div className="main-container">
     <h1>Topics</h1>
-    <ul class="topics">
-      {props.topics.map(topic => (
-        <li class="topic"><a href="/topics/:id">{topic.title}</a></li>
-      ))}
-    </ul>
+    <Topics {...props} />
   </div>
 );
 
-export connect(Home
+export const Home = connect(mapStateToProps, mapDispatchToProps)(HomePresenter);
