7aa0083558615966924da1e2904a03b540742194
Jeremy Meyer <jeremythedev@gmail.com>
2017-01-16 13:35:04 -0800

    style: add whitespace

--- src/client/app.js
+++ src/client/app.js
@@ -2,6 +2,7 @@ const feathers = require('feathers-client');
 const io = require('socket.io-client');
 
 const socket = io();
+
 export const app = feathers()
   .configure(feathers.socketio(socket))
   .configure(feathers.hooks())
